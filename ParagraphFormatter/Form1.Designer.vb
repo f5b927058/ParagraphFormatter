b945930b64576936b718a75c18bf7890080f1354
f5b927058 <f5b927058@gmail.com>
2024-12-08 18:46:50 +0600

    1st commit

---
+++ ParagraphFormatter/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.TextBox2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(727, 486)
+        Me.SplitContainer1.SplitterDistance = 362
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(362, 486)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(361, 486)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(727, 486)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Font = New System.Drawing.Font("Meiryo UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.Text = "ParagraphFormatter"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ ParagraphFormatter/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        Dim s = Me.TextBox1.Text.Trim()
+
+        Dim paragraphs = New List(Of String)
+        Dim sb_paragraph = New Text.StringBuilder
+
+        For Each s_line In s.Split(vbCrLf)
+            s_line = s_line.Trim()
+            If s_line = "" Then
+                ' 空白行
+                If sb_paragraph.Length = 0 Then
+                    Continue For
+                End If
+                paragraphs.Add(sb_paragraph.ToString().Trim())
+                sb_paragraph.Clear()
+
+            Else
+                ' 空白行でない
+                If Not (sb_paragraph.Length > 0 AndAlso sb_paragraph.ToString().Last() = "-") Then
+                    sb_paragraph.Append(" ")
+                End If
+                sb_paragraph.Append(s_line)
+
+            End If
+        Next
+        If sb_paragraph.Length > 0 Then
+            paragraphs.Add(sb_paragraph.ToString().Trim())
+        End If
+
+        Dim sb = New Text.StringBuilder()
+        Dim n = 0
+        Dim sb_section = New Text.StringBuilder()
+
+        For Each s_para In paragraphs
+            If n + s_para.Length > 4900 Then
+                If sb_section.Length > 0 Then
+                    If sb.Length > 0 Then
+                        sb.AppendLine()
+                        sb.AppendLine("########################################")
+                        sb.AppendLine()
+                    End If
+                    sb.AppendLine(sb_section.ToString().Trim())
+                    sb_section.Clear()
+                    sb_section.AppendLine(s_para)
+                    n = s_para.Length
+                    Continue For
+                End If
+            End If
+
+            n += s_para.Length
+            sb_section.Append(s_para & vbCrLf & vbCrLf)
+
+        Next
+        If sb_section.Length > 0 Then
+            If sb.Length > 0 Then
+                sb.AppendLine()
+                sb.AppendLine("########################################")
+                sb.AppendLine()
+            End If
+            sb.AppendLine(sb_section.ToString().Trim())
+        End If
+        Me.TextBox2.Text = sb.ToString()
+
+    End Sub
+
+
+End Class
